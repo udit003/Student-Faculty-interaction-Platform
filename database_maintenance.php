9a0ab44178a68029f1c93fd39fb5da6bb93040ac
udit003 <uditagarwal003@gmail.com>
2017-10-23 01:22:08 +0530

    created file forsystem maintenance code

---
+++ database_maintenance.php
@@ -0,0 +1,47 @@
+<?php
+	/* This is a system maintenance file, used for adding departments etc .*/ 
+	$servername="localhost";
+	$username="root";
+	$password="rangrezz";
+	$conn=new mysqli($servername,$username,$password);
+	if ($conn->connect_error) 
+	{
+    die("Connection failed: " . $conn->connect_error);
+	} 
+		$sql2="use STIP";
+
+	if ($conn->query($sql2) === TRUE) {
+	echo "successfull\n";
+	} else {
+		echo "Unsuccessfull" . $conn->error;
+	}	
+
+
+	$sql1="insert into departments (department_name)
+			values('Computer Science and Engineering'),
+			('Electrical and Electronics Engineering'),
+			('Electronics and Communication Engineering'),
+			('Production Engineering'),
+			('Instrumentation and Control Engineering'),
+			('Mechanical Engineering'),
+			('Civil Engineering'),
+			('Department of Architecture'),
+			('Chemical Engineering'),
+			('Computer Applications'),
+			('Department of Chemistry'),
+			('Metallurgical and Materials Engineering'),
+			('Department of Physics'),
+			('Department of Mathematics'),
+			('Management Studies'),
+			('CECASE'),
+			('DEE'),
+			('Humanities');";
+
+		if ($conn->query($sql1) === TRUE) {
+	echo "new departments added successfully\n";
+	} else {
+		echo "Error inserting into table: " . $conn->error;
+	}
+
+	$conn->close();	
+?>
