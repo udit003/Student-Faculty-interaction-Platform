d0bb2d7edc945b835a8424e4ea7b19db0aad390f
VaibhavN007 <vaibhavnit007@gmail.com>
2017-11-17 02:46:42 +0530

    Add create assignment page

---
+++ js/create_assignment.js
@@ -0,0 +1,39 @@
+$("document").ready(function() {
+	Date.prototype.addDays = function(days) {
+		this.setDate(this.getDate() + parseInt(days));
+		return this;
+	};
+
+	let now = new Date();
+	let assign_start_elmt = $("#assign-start-date"),
+		assign_end_elmt = $("#assign-end-date"),
+		today_date = now.toISOString().substr(0, 10),
+		start_date = today_date,
+		end_date = (new Date()).addDays(7).toISOString().substr(0, 10);
+
+	assign_start_elmt.val(start_date).prop("min", today_date);
+	assign_end_elmt.val(end_date).prop("min", today_date);
+
+	assign_start_elmt.on("mouseup change", function(e) {
+		start_date = $(this).val();
+		if(start_date < today_date) {
+			start_date = today_date;
+			assign_start_elmt.val(today_date);
+		}
+		if(start_date > end_date) {
+			end_date = start_date;
+			assign_end_elmt.val(end_date).prop("min", end_date);
+		} else {
+			assign_end_elmt.prop("min", start_date);
+		}
+	});
+
+	assign_end_elmt.on("mouseup change", function() {
+		end_date = $(this).val();
+		if (end_date < start_date) {
+			end_date = start_date;
+			assign_end_elmt.val(end_date).prop("min", end_date);
+		}
+	});
+
+});
